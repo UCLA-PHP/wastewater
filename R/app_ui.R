a465d51711eb5688420fb7c3fb45282768ce81bb
dcp14 <derek.pham14@gmail.com>
2023-08-23 11:54:45 -0700

    Removed wastewater.dcf and concise fullscreen

--- R/app_ui.R
+++ R/app_ui.R
@@ -113,26 +113,7 @@ app_ui <- function(request) {
                 label = "PMMoV Normalized",
                 value = TRUE),
               htmltools::htmlDependencies(icon("")),
-              tags$style('
-        .plot-zoom {
-            position: absolute;
-            border: none;
-            background-color: transparent;
-            bottom: 0;
-            right: 0;
-
-        }
-        .full-screen {
-            position: fixed;
-            height: 98vh !important;
-            width: 98vw !important;
-            left: 0;
-            top: 0;
-            z-index: 9999;
-            overflow: hidden;
-        }
-        '
-              ),
+              tags$style(fullscreen_tag1),
               div(
                 class = "plotly-full-screen",
               fluidRow(
@@ -160,32 +141,7 @@ app_ui <- function(request) {
               )
             )
           ),
-          tags$script(HTML("
-        function plotZoom(el){
-            el = $(el);
-            var parent = el.parent().parent();
-            if(el.attr('data-full_screen') === 'false') {
-                parent.addClass('full-screen').trigger('resize').fadeOut().fadeIn();
-                el.attr('data-full_screen', 'true');
-            } else {
-                parent.removeClass('full-screen').trigger('resize').fadeOut().fadeIn();
-                el.attr('data-full_screen', 'false');
-            }
-        }
-
-        $(function(){
-           $('.plotly-full-screen  .plotly.html-widget').append(
-            `
-            <div style='position: relative;'>
-                <button onclick=plotZoom(this) class='plot-zoom' data-full_screen='false' title='Full screen'>
-                    <i class='fa fa-expand-arrows-alt'></i>
-                </button>
-            </div>
-            `);
-        })
-        "))
-
-
+          tags$script(HTML(fullscreen_tag2))
           )
         )
       )
